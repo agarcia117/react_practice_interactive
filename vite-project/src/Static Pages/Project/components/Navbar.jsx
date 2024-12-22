76dad7891a03edf09b69eda85ec4fb84c0805eb6
Alan Garcia <agarcia13777@gmail.com>
2024-12-22 11:44:10 -0800

    Completed Styling and setup for Header for Static Page project

--- vite-project/src/Static Pages/Project/components/Navbar.jsx
+++ vite-project/src/Static Pages/Project/components/Navbar.jsx
@@ -1,6 +1,13 @@
+import reactLogo from '../../Fragments/React-icon.png'
+
 function Navbar(){
   return(
-    <h1>Navbar goes here</h1>
+    <header className='header'>
+      <nav className='header-nav'>
+        <img src={reactLogo} className='header-icon' alt="React Logo" />
+        <h1>ReactFacts</h1>
+      </nav>
+    </header>
   )
 }
 
