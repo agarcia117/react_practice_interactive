bb554d480753c3268e75c8d4c28769cb34d9c762
Alan Garcia <agarcia13777@gmail.com>
2024-12-24 12:27:31 -0800

    Completed the travel journal project

--- vite-project/src/DataDriven-React/TravelJournal/index.jsx
+++ vite-project/src/DataDriven-React/TravelJournal/index.jsx
@@ -6,12 +6,7 @@ function Page() {
   const entryElements = data.map(entry => {
     return <Entry
               key={entry.id} 
-              img={entry.img} 
-              title={entry.title} 
-              country={entry.country} 
-              googleMapsLink={entry.googleMapsLink} 
-              dates={entry.dates} 
-              text={entry.text}
+              {...entry}
             />;
   });
 
