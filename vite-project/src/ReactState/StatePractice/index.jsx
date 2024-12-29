2b29137616a048a1fbe988778f54e7e33646e1a5
Alan Garcia <agarcia13777@gmail.com>
2024-12-28 16:30:47 -0800

    Finished Radio Button section

--- vite-project/src/ReactState/StatePractice/index.jsx
+++ vite-project/src/ReactState/StatePractice/index.jsx
@@ -8,8 +8,11 @@ function Page() {
   function signUp(formData){
     const email = formData.get("email");
     const password = formData.get("password");
+    const employmentStatus = formData.get("employmentStatus");
     console.log(email);
     console.log(password);
+    console.log(employmentStatus);
+    
     
   }
   
@@ -19,13 +22,32 @@ function Page() {
           <h1>Signup form</h1>
           <form action={signUp}>
             <label htmlFor="email">Email:</label>
-            <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-            <br />
-            
-            <label htmlFor="password">Password:</label>
-            <input id="password" type="password" name="password"/>
-            <br />
+            <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
 
+            <label htmlFor="password">Password:</label>
+            <input id="password" defaultValue="password123" type="password" name="password" />
+
+            <label htmlFor="description">Description:</label>
+            <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+
+            <fieldset>
+              <legend>Employment Status:</legend>
+              <label>
+                <input type="radio" name="employmentStatus" value="unemployed" />
+                Unemployed
+            </label>
+              <label>
+                <input type="radio" name="employmentStatus" value="part-time"/>
+                Part-time
+            </label>
+              <label>
+                <input type="radio" name="employmentStatus" defaultChecked={true} value="full-time"/>
+                Full-time
+            </label>
+            </fieldset>
+                
+
+            
             <button>Submit</button>
             
           </form>
