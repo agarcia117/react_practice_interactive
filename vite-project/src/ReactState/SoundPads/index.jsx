7bb42ccf5d14db02f732b13275e003cd614a5bce
Alan Garcia <agarcia13777@gmail.com>
2025-01-01 14:17:40 -0800

    Changed method of handling toggle in SoundPads

--- vite-project/src/ReactState/SoundPads/index.jsx
+++ vite-project/src/ReactState/SoundPads/index.jsx
@@ -10,10 +10,15 @@ function Page(props) {
   });
 
   function toggle(id){
+    
     setPadsArr(prevArr => {
       return prevArr.map(pad => {
-        return pad.id === id ? {...pad, on: !pad.on} : pad;
-      });
+        if(pad.id === id){
+          return {...pad, on:!pad.on};
+        }
+        
+        return pad;
+      })
     });
   }
   
@@ -24,169 +29,7 @@ function Page(props) {
             {padsElement}
         </div>
     </main>
-)
+  )
 }
 
 export default Page
-
-
-
-
-
-// const [count, setCount] = useState(0);
-
-// function decrementCount(){
-//   setCount(prevCount => prevCount - 1);
-// }
-
-// function incrementCount(){
-//   setCount(prevCount => prevCount + 1);
-// }
-{/* <main className="container">
-  <div className="counter">
-      <button onClick={decrementCount} className="minus" aria-label="Decrease count">–</button>
-      <Count number={count}/>
-      <button onClick={incrementCount} className="plus" aria-label="Increase count">+</button>
-  </div>
-</main> */}
-
-
-
-
-// const [isGoingOut, setIsGoingOut] = useState(false);
-  
-// function flipAnswer(){
-//   setIsGoingOut(prevBoolean => !prevBoolean);
-// }
-// <main>
-//   <h1 className="title">Do I feel like going out tonight?</h1>
-//   <button onClick={flipAnswer} className="value" aria-label={`Current Answer is ${isGoingOut ? "Yes":"No"}. Click to change it.`}>{isGoingOut ? "Yes":"No"}</button>
-// </main>
-
-
-
-
-
-// const [myFavoriteThings, setMyFavoriteThings] = useState([]);
-// //const myFavoriteThings = []
-// const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁", 
-// "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
-// const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
-// function addFavoriteThing() {
-//   setMyFavoriteThings(prev => [...prev, allFavoriteThings[prev.length]])
-// }
-{/* <main>
-          <button onClick={addFavoriteThing}>Add item</button>
-          <section aria-live="polite">
-            {thingsElements}
-          </section>
-        </main> */}
-
-
-
-
-// const [contact, setContact] = useState({
-//   firstName: "John",
-//   lastName: "Doe",
-//   phone: "+1 (212) 555-1212",
-//   email: "itsmyrealname@example.com",
-//   isFavorite: false
-// });
-// function toggleFavorite() {
-//     setContact(prevContact => {
-//       return {...prevContact, isFavorite : !prevContact.isFavorite}
-//     })
-// }
-// <main>
-//   <article className="card">
-//       <img
-//           src={avatar}
-//           className="avatar"
-//           alt="User profile picture of John Doe"
-//       />
-//       <div className="info">
-//           <Star isFilled={contact.isFavorite} handleClick={toggleFavorite}/>
-//           <h2 className="name">
-//               {`${contact.firstName} ${contact.lastName}`}
-//           </h2>
-//           <p className="contact">{contact.phone}</p>
-//           <p className="contact">{contact.email}</p>
-//       </div>
-//   </article>
-// </main>
-
-
-
-
-
-
-
-// function signUp(formData){
-//   const data = Object.fromEntries(formData);
-//   const dietaryRestrictions = formData.getAll("dietaryRestrictions");
-//   const allData = {
-//     ...data,
-//     dietaryRestrictions
-//   }
-//   console.log(allData);
-// }
-{/* <section>
-          <h1>Signup form</h1>
-          <form action={signUp}>
-            <label htmlFor="email">Email:</label>
-            <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
-
-            <label htmlFor="password">Password:</label>
-            <input id="password" defaultValue="password123" type="password" name="password" />
-
-            <label htmlFor="description">Description:</label>
-            <textarea id="description" name="description" defaultValue="This is a description"></textarea>
-
-            <fieldset>
-              <legend>Employment Status:</legend>
-              <label>
-                <input type="radio" name="employmentStatus" value="unemployed" />
-                Unemployed
-            </label>
-              <label>
-                <input type="radio" name="employmentStatus" value="part-time"/>
-                Part-time
-            </label>
-              <label>
-                <input type="radio" name="employmentStatus" defaultChecked={true} value="full-time"/>
-                Full-time
-            </label>
-            </fieldset>
-            
-            <fieldset>
-              <legend>Dietary restrictions:</legend>
-              <label>
-                <input type="checkbox" name="dietaryRestrictions" value="kosher" />
-                Kosher
-            </label>
-              <label>
-                <input type="checkbox" name="dietaryRestrictions" value="vegan" />
-                Vegan
-            </label>
-              <label>
-                <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="gluten-free" />
-                Gluten-free
-            </label>
-            </fieldset>
-
-            <label htmlFor="favColor">What is your favorite color?</label>
-            <select id="favColor" name="favColor" defaultValue="" required>
-              <option value="" disabled>-- Choose a color --</option>
-              <option value="red">Red</option>
-              <option value="orange">Orange</option>
-              <option value="yellow">Yellow</option>
-              <option value="green">Green</option>
-              <option value="blue">Blue</option>
-              <option value="indigo">Indigo</option>
-              <option value="violet">Violet</option>
-            </select>
-            
-            <button>Submit</button>
-            
-          </form>
-        </section> */}
