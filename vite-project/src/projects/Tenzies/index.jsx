c8f82d23090de82bb79b4163253ba1bd53b5892e
Alan Garcia <agarcia13777@gmail.com>
2025-01-19 11:04:39 -0800

    Added to Confetti when game is won

--- vite-project/src/projects/Tenzies/index.jsx
+++ vite-project/src/projects/Tenzies/index.jsx
@@ -1,17 +1,20 @@
 import Die from "./components/Die.jsx"
 import { useState } from "react"
 import {nanoid} from "nanoid"
+import ReactConfetti from "react-confetti";
 
 function Page() {
-  const [dice, setDice] = useState(generateAllNewDie());
+  const [dice, setDice] = useState(() => generateAllNewDie());
 
+  let gameWon = false;
   if(dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)){
-    console.log("Game is Over");
+    gameWon = true;
   }
 
   function generateAllNewDie() {
     let diceArray = [];
-
+    console.log("generate");
+    
     for(let i = 0; i < 10; i++){
       diceArray.push({value: Math.ceil(Math.random() * 6), isHeld:false, id: nanoid()})
     }      
@@ -42,13 +45,14 @@ function Page() {
   return (
       <>
         <main>
+          {gameWon && <ReactConfetti/>}
           <h1 className="title">Tenzies</h1>
           <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <div className="container">
             {diceElements}
           </div>
 
-          <button onClick={rerollDice} className="roll-button">Roll</button>
+          <button onClick={rerollDice} className="roll-button">{gameWon ? "New Game" : "Roll"}</button>
         </main>
       </>
   )
