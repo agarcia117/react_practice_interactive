8757e787425fa01423f17950228a0d63e9d57321
Alan Garcia <agarcia13777@gmail.com>
2025-01-19 17:57:04 -0800

    Completed color coding on right and wrong keyboard clicks

--- vite-project/src/projects/AssemblyEndgame/index.jsx
+++ vite-project/src/projects/AssemblyEndgame/index.jsx
@@ -1,19 +1,69 @@
 import {useState } from "react"
+import { languages } from "./languages"
+import { clsx } from "clsx";
 
 function Page() {
+
+  const [currentWord, setCurrentWord] = useState('react');
+  const [guesses, setGuesses] = useState([]);
+  console.log(guesses);
+  
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const languagesElements = languages.map(languageObj => {
+    const styles = {
+      backgroundColor: languageObj.backgroundColor,
+      color: languageObj.color
+    }
+    return <span style={styles} key={languageObj.name}>{languageObj.name}</span>;
+  });
+
+  const letterElements = currentWord.split('').map((letter, index) => {
+    return <span key={index}>{letter.toUpperCase()}</span>
+  });
+
+  const keyboardElements = alphabet.split('').map(letter => {
+    const isGuessed = guesses.includes(letter);
+    const isCorrect = currentWord.includes(letter) && isGuessed;
+    const isWrong = !currentWord.includes(letter) && isGuessed;
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong
+    });
+    
+    return <button className={className} onClick={() => addToGuess(letter)} key={letter}>{letter.toUpperCase()}</button>
+  });
+
+  function addToGuess(letter){
+    setGuesses(prevGuess =>
+      prevGuess.includes(letter) ? prevGuess : [...prevGuess, letter]
+    )
+  }
   
   return (
       <>
         <main>
-            <header>
-              <h1>Assembly: Endgame</h1>
-              <p>Guess the word in under 8 attempts to keep the programming world safe from assembly!</p>
-            </header>
-            <section className="status-container">
-              <h1>You Win!</h1>
-              <p>Well done! &#127881;</p>
-            </section>
-          
+          <header>
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from assembly!</p>
+          </header>
+          <section className="status-container">
+            <h1>You Win!</h1>
+            <p>Well done! &#127881;</p>
+          </section>
+
+          <section className="languages-container">
+            {languagesElements}
+          </section>
+
+          <section className="letters-container">
+            {letterElements}
+          </section>
+
+          <section className="keyboard-container">
+            {keyboardElements}
+          </section>
+          <button className="new-game">New Game</button>
         </main>
       </>
   )
